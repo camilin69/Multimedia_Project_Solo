517b7dace4e07ae4b8f228a03b212ad176e55605
camilin69 <camilo.merchan02@uptc.edu.co>
2024-02-20 12:41:31 -0500

    Let the CRUD for series admin functionalities.

--- src/main/java/co/edu/uptc/controller/GenreController.java
+++ src/main/java/co/edu/uptc/controller/GenreController.java
@@ -1,6 +1,110 @@
 package co.edu.uptc.controller;
 
+import java.util.ArrayList;
+
+import co.edu.uptc.model.Genre;
+import co.edu.uptc.model.Movie;
+import co.edu.uptc.model.Serie;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class GenreController {
+    ArrayList<Genre> genres = new ArrayList<>();
 
+    public void createGenre(String name, ArrayList<Movie> moviesToAdd, ArrayList<Serie> seriesToAdd, String textCover, String backgroundCover){
+        Genre newGenre = new Genre(name);
+        newGenre.setMovies(moviesToAdd);
+        newGenre.setSeries(seriesToAdd);
+        newGenre.setTextCover(textCover);
+        newGenre.setBackgroundCover(backgroundCover);
+        genres.add(newGenre);
+    }
+
+    public int verifyInputs(TextField genreNameField, Label genreNameLabel){
+        int aux = 0;
+
+        if(genreNameField.getText().matches("^[a-zA-Z\\s]+$")){
+            aux++;
+            genreNameLabel.setTextFill(Color.web("#807170"));
+            if(!genreFound(genreNameField.getText())){
+                aux++;
+            }else{
+                genreNameLabel.setText("Genres can not be repeated");
+                genreNameLabel.setTextFill(Color.RED);
+            }
+        }else{
+            genreNameLabel.setText("Genre name only contains characters");
+            genreNameLabel.setTextFill(Color.RED);
+        }
+        return aux;
+    }
+
+    
+
+    public boolean genreFound(String genreName){
+        for(Genre genre : genres){
+            if(genre.getName().equals(genreName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    
+
+    public ArrayList<Serie> setSeries(ObservableList<String> genreSeries, ArrayList<Serie> allSeries){
+        ArrayList<Serie> series = new ArrayList<>();
+        SerieController sc = new SerieController();
+
+        for(String i : genreSeries){
+            Serie newSerie = sc.currentSerie(allSeries, i);
+            series.add(newSerie);
+        }
+        return series;
+    }
+
+
+    
+
+    public ObservableList<String> currentGenreSeries(ComboBox<String> currentGenreSeries, ArrayList<Serie> series){
+        ObservableList<String> aux = FXCollections.observableArrayList();
+        for(Serie serie: series){
+            aux.add(serie.getName());
+        }
+        return aux;
+    }
+
+    public ObservableList<String> allSeriesAvailable(ObservableList<String> currentGenreSeries, ArrayList<Serie> allSeries){
+        ObservableList<String> series = FXCollections.observableArrayList();
+        for(Serie serie: allSeries){series.add(serie.getName());}
+
+        for(String serie: currentGenreSeries){
+            series.remove(serie);
+        }
+        return series;
+    }
+
+
+
+
+
+
+    public ArrayList<Genre> getGenres() {
+        return genres;
+    }
+    public void setGenres(ArrayList<Genre> genres) {
+        this.genres = genres;
+    }
+    public void addGenre(Genre genre){
+        genres.add(genre);
+    }
+    public void removeGenre(Genre genre){
+        genres.remove(genre);
+    }
+
+    
 }
--- src/main/java/co/edu/uptc/controller/UserController.java
+++ src/main/java/co/edu/uptc/controller/UserController.java
@@ -1,6 +1,204 @@
 package co.edu.uptc.controller;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import co.edu.uptc.model.UserRegistered;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class UserController {
+    private ArrayList<UserRegistered> users = new ArrayList<>();
+    private UserRegistered currentUser;
+
+    public void createUser(String firstName, String lastName, int id, String email, String password){
+        UserRegistered newUser = new UserRegistered(firstName, lastName, id, email, password);
+        users.add(newUser);
+
+    }
+
+    public int verifyRegisterUserInputs(TextField firstNameField, Label firstNameLabel,
+    TextField lastNameField, Label lastNameLabel, TextField emailRegisterField, Label emailRegisterLabel,
+    TextField passwordRegisterField, Label minCharactersLabel, Label specialCharactersLabel, Label uppercaseLabel,
+    Label lowercaseLabel, Label minNumberLabel, TextField passwordToMatchField, Label passwordToMatchLabel){
+        int aux = 0;
+        if(firstNameField.getText().matches("^[a-zA-Z\\s]+$")){
+            aux++;
+            firstNameLabel.setTextFill(Color.web("#cda6ff"));
+        }else{
+           firstNameLabel.setTextFill(Color.RED); 
+        }
+        if(lastNameField.getText().matches("^[a-zA-Z\\s]+$")){
+            aux++;
+            lastNameLabel.setTextFill(Color.web("#cda6ff"));
+        }else{
+           lastNameLabel.setTextFill(Color.RED); 
+        }
+        
+        if(emailRegisterField.getText().contains("@uptc.edu.co")){  
+            aux++;
+            emailRegisterLabel.setTextFill(Color.web("#cda6ff"));
+        }else{
+            emailRegisterLabel.setTextFill(Color.RED);
+        }
+        if(passwordRegisterField.getText().matches(".{8,}$")){
+            minCharactersLabel.setTextFill(Color.web("#cda6ff"));
+            aux++;
+        }else{
+            minCharactersLabel.setTextFill(Color.RED);
+        }
+        if(verifyPasswordAux(passwordRegisterField.getText(), "[!@#$%^&*(),.?\":{}|<>]")){
+            aux++;
+            specialCharactersLabel.setTextFill(Color.web("#cda6ff"));
+        }else{
+            specialCharactersLabel.setTextFill(Color.RED);
+        }
+        if(verifyPasswordAux(passwordRegisterField.getText(), "[A-Z]")){
+            aux++;
+            uppercaseLabel.setTextFill(Color.web("#cda6ff"));
+        }else{
+            uppercaseLabel.setTextFill(Color.RED);
+        }
+        if(verifyPasswordAux(passwordRegisterField.getText(), "[a-z]")){
+            aux++;
+            lowercaseLabel.setTextFill(Color.web("#cda6ff"));
+        }else{
+            lowercaseLabel.setTextFill(Color.RED);
+        }
+        if(verifyPasswordAux(passwordRegisterField.getText(), "[0-9]")){
+            aux++;
+            minNumberLabel.setTextFill(Color.web("#cda6ff"));
+        }else{
+            minNumberLabel.setTextFill(Color.RED);
+        }
+        if(passwordRegisterField.getText().equals(passwordToMatchField.getText())){
+            passwordToMatchLabel.setTextFill(Color.web("#cda6ff"));
+            aux++;
+        }else{
+            passwordToMatchLabel.setTextFill(Color.RED);
+        }
+
+        return aux;
+    }
+
+    public int assignidUser() {
+        int aux = 0;
+        ArrayList<Integer> ids = new ArrayList<>();
+        for (UserRegistered u : users) {
+            ids.add(u.getId());
+        }
+        while (true && aux == 0) {
+            aux = (int) (Math.random() * 1000);
+            if (!ids.contains(aux)) {
+                break;
+            }
+        }
+        return aux;
+    }
+
+    public void updateUsers(UserRegistered currentUser){
+        for(UserRegistered user: users){
+            if(user.getId() == currentUser.getId()){
+                users.remove(user);
+                users.add(currentUser);
+                break;
+            }
+        }
+    }
+    public boolean verifyPasswordAux(String password, String regex){
+        boolean rta = false;
+        char[] passwordChar = password.toCharArray();
+        for(int i = 0; i < passwordChar.length && true; i++){
+            if(String.valueOf(passwordChar[i]).matches(regex)){
+                rta = true;
+                break;
+            }else if(i == passwordChar.length-1){return false;}
+        }
+        return rta;
+    }
+
+    public void saveUsersToJson(){
+        try (FileWriter writer = new FileWriter(System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/admin-user/users.json")) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(users, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveCurrentUserToJson(){
+        try (FileWriter writer = new FileWriter(System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/admin-user/currentUser.json")) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(currentUser, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<UserRegistered> loadUsersFromJson() {
+        ArrayList<UserRegistered> users = new ArrayList<>();
+
+        try (Reader reader = new FileReader(System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/admin-user/users.json")) {
+
+            Type listType = new TypeToken<List<UserRegistered>>(){}.getType();
+            users = new Gson().fromJson(reader, listType);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    public UserRegistered loadCurrentUserFromJson() {
+        UserRegistered currentUser = new UserRegistered();
+
+        try (Reader reader = new FileReader(System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/admin-user/currentUser.json")) {
+
+            Type listType = new TypeToken<UserRegistered>(){}.getType();
+            currentUser = new Gson().fromJson(reader, listType);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return currentUser;
+    }
+
+
     
+
+    public boolean userFound(String email){
+        for(UserRegistered user: users){
+            if(user.couldLogIn(email)){
+                currentUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+    public ArrayList<UserRegistered> getUsers() {
+        return users;
+    }
+
+    public UserRegistered getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setUsers(ArrayList<UserRegistered> users) {
+        this.users = users;
+    }
+
+    public void setCurrentUser(UserRegistered currentUser) {
+        this.currentUser = currentUser;
+    }
 }
