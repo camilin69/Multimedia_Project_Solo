4eb0f8c4cc6e4e8e1fe9c83dce13762d78f04324
camilin69 <camilo.merchan02@uptc.edu.co>
2024-02-25 21:43:30 -0500

    Let the subscription merge done!

--- src/main/java/co/edu/uptc/controller/SubscriptionController.java
+++ src/main/java/co/edu/uptc/controller/SubscriptionController.java
@@ -1,6 +1,246 @@
 package co.edu.uptc.controller;
 
+import java.util.ArrayList;
+
+import co.edu.uptc.model.Subscription;
+import co.edu.uptc.model.UserRegistered;
+import co.edu.uptc.model.UserSubscription;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class SubscriptionController {
+    ArrayList<Subscription> subscriptions = new ArrayList<>();
 
+    public void createSubscription(String name, String description, int duration, double price, String cover, String path){
+        Subscription newSub = new Subscription(name, duration, description, price, cover, path);
+        subscriptions.add(newSub);
+    }
+
+    public void addSubToUser(Subscription sub, UserRegistered user){
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + sub.getDuration();  
+        UserSubscription userSub = new UserSubscription(sub.getName(), sub.getDuration(), sub.getDescription(),
+                         sub.getPrice(), sub.getCoverURL(), sub.getImageLocalPathURL(),
+                         startTime, endTime);
+        user.setSub(userSub);
+    }
+
+    public void cancelSub(UserRegistered user){
+        user.setSub(null);
+    }
+
+    public int verifyInputs(TextField subscriptionNameField, Label subscriptionNameLabel,
+                            TextField subscriptionDescriptionField, Label subscriptionDescriptionLabel,
+                            TextField subscriptionDurationField, Label subscriptionDurationLabel,
+                            TextField subscriptionPriceField, Label subscriptionPriceLabel,
+                            TextField subscriptionCoverField, Label subscriptionCoverLabel){
+
+        int aux = 0;
+
+        if(subscriptionNameField.getText().matches("^[a-zA-Z0-9\\s]+$")){
+            aux++;
+            subscriptionNameLabel.setTextFill(Color.web("#5483B3"));
+            if(!subscriptionFound(subscriptionNameField.getText())){
+                aux++;
+                subscriptionNameLabel.setTextFill(Color.web("#5483B3"));
+            }else{
+                subscriptionNameLabel.setText("Subscriptions can not be repeated");
+                subscriptionNameField.setOpacity(40);
+                subscriptionNameField.setStyle("-fx-background-color: #ff7a7a;");
+                Timeline effect = new Timeline(
+                    new KeyFrame(Duration.seconds(2), event -> {subscriptionNameField.setStyle("-fx-background-color: #ffffff;");subscriptionNameField.setOpacity(100);})
+                );
+                effect.setCycleCount(1);
+                effect.play();
+                subscriptionNameLabel.setTextFill(Color.RED);
+            }
+        }else{
+            subscriptionNameLabel.setText("Names only contains characters");
+            subscriptionNameField.setOpacity(40);
+            subscriptionNameField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionNameField.setStyle("-fx-background-color: #ffffff;");subscriptionNameField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionNameLabel.setTextFill(Color.RED);
+        }
+        if(subscriptionDescriptionField.getText().length() > 10){
+            aux++;
+            subscriptionDescriptionLabel.setTextFill(Color.web("#5483B3"));
+        }else{
+            subscriptionDescriptionField.setOpacity(40);
+            subscriptionDescriptionField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionDescriptionField.setStyle("-fx-background-color: #ffffff;");subscriptionDescriptionField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionDescriptionLabel.setTextFill(Color.RED);
+        }
+        if(subscriptionDurationField.getText().matches("\\d+")){
+            aux++;
+            subscriptionDurationLabel.setTextFill(Color.web("#5483B3"));
+        }else{
+            subscriptionDurationField.setOpacity(40);
+            subscriptionDurationField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionDurationField.setStyle("-fx-background-color: #ffffff;");subscriptionDurationField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionDurationLabel.setTextFill(Color.RED);
+        }
+        try{
+            Double.parseDouble(subscriptionPriceField.getText());
+            subscriptionPriceLabel.setTextFill(Color.web("#5483B3"));
+            aux++;
+        }catch(Exception e){
+            subscriptionPriceField.setOpacity(40);
+            subscriptionPriceField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionPriceField.setStyle("-fx-background-color: #ffffff;");subscriptionPriceField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionPriceLabel.setTextFill(Color.RED);
+        }
+        if(subscriptionCoverField.getText().startsWith("https://")){
+            subscriptionCoverLabel.setTextFill(Color.web("#5483B3"));
+            aux++;
+        }else{
+            subscriptionCoverField.setOpacity(40);
+            subscriptionCoverField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionCoverField.setStyle("-fx-background-color: #ffffff;");subscriptionCoverField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionCoverLabel.setTextFill(Color.RED);
+        }
+        return aux;
+    }
+
+    public int verifyInputsToEdit(String oldName, TextField subscriptionNameField, Label subscriptionNameLabel,
+                            TextField subscriptionDescriptionField, Label subscriptionDescriptionLabel,
+                            TextField subscriptionDurationField, Label subscriptionDurationLabel,
+                            TextField subscriptionPriceField, Label subscriptionPriceLabel,
+                            TextField subscriptionCoverField, Label subscriptionCoverLabel){
+
+        int aux = 0;
+
+        if(subscriptionNameField.getText().matches("^[a-zA-Z0-9\\s]+$")){
+            aux++;
+            subscriptionNameLabel.setTextFill(Color.web("#5483B3a"));
+            if(oldName.equals(subscriptionNameField.getText())){
+                aux++;
+            }else{
+                if(!subscriptionFound(subscriptionNameField.getText())){
+                    aux++;
+                    subscriptionNameLabel.setTextFill(Color.web("#5483B3a"));
+                }else{
+                    subscriptionNameLabel.setText("Subscriptions can not be repeated");
+                    subscriptionNameField.setOpacity(40);
+                    subscriptionNameField.setStyle("-fx-background-color: #ff7a7a;");
+                    Timeline effect = new Timeline(
+                        new KeyFrame(Duration.seconds(2), event -> {subscriptionNameField.setStyle("-fx-background-color: #ffffff;");subscriptionNameField.setOpacity(100);})
+                    );
+                    effect.setCycleCount(1);
+                    effect.play();
+                    subscriptionNameLabel.setTextFill(Color.RED);
+                }
+            }
+            
+        }else{
+            subscriptionNameLabel.setText("Names only contains characters");
+            subscriptionNameField.setOpacity(40);
+            subscriptionNameField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionNameField.setStyle("-fx-background-color: #ffffff;");subscriptionNameField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionNameLabel.setTextFill(Color.RED);
+        }
+        if(subscriptionDescriptionField.getText().matches("^[a-zA-Z0-9\\s\\p{Punct}]+$")){
+            aux++;
+            subscriptionDescriptionLabel.setTextFill(Color.web("#5483B3a"));
+        }else{
+            subscriptionNameLabel.setText("Names only contains characters");
+            subscriptionDescriptionField.setOpacity(40);
+            subscriptionDescriptionField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionDescriptionField.setStyle("-fx-background-color: #ffffff;");subscriptionDescriptionField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionDescriptionLabel.setTextFill(Color.RED);
+        }
+        if(subscriptionDurationField.getText().matches("\\d+")){
+            aux++;
+            subscriptionDurationLabel.setTextFill(Color.web("#5483B3a"));
+        }else{
+            subscriptionDurationField.setOpacity(40);
+            subscriptionDurationField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionDurationField.setStyle("-fx-background-color: #ffffff;");subscriptionDurationField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionDurationLabel.setTextFill(Color.RED);
+        }
+        try{
+            Double.parseDouble(subscriptionPriceField.getText());
+            subscriptionPriceLabel.setTextFill(Color.web("#5483B3a"));
+            aux++;
+        }catch(Exception e){
+            subscriptionPriceField.setOpacity(40);
+            subscriptionPriceField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionPriceField.setStyle("-fx-background-color: #ffffff;");subscriptionPriceField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionPriceLabel.setTextFill(Color.RED);
+        }
+        if(subscriptionCoverField.getText().startsWith("https://")){
+            subscriptionCoverLabel.setTextFill(Color.web("#5483B3a"));
+            aux++;
+        }else{
+            subscriptionCoverField.setOpacity(40);
+            subscriptionCoverField.setStyle("-fx-background-color: #ff7a7a;");
+            Timeline effect = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {subscriptionCoverField.setStyle("-fx-background-color: #ffffff;");subscriptionCoverField.setOpacity(100);})
+            );
+            effect.setCycleCount(1);
+            effect.play();
+            subscriptionCoverLabel.setTextFill(Color.RED);
+        }
+        return aux;
+    }
+
+    public boolean subscriptionFound(String subName){
+        for(Subscription sub: subscriptions){
+            if(sub.getName().equals(subName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Subscription> getSubscriptions() {
+        return subscriptions;
+    }
+    public void setSubscriptions(ArrayList<Subscription> subscriptions) {
+        this.subscriptions = subscriptions;
+    }
+    public void removeSubscription(Subscription sub){
+        subscriptions.remove(sub);
+    }
+
+    
 }
--- src/main/java/co/edu/uptc/view/AdminView.java
+++ src/main/java/co/edu/uptc/view/AdminView.java
@@ -10,6 +10,7 @@ import co.edu.uptc.controller.AdminController;
 import co.edu.uptc.controller.GenreController;
 import co.edu.uptc.controller.MovieController;
 import co.edu.uptc.controller.SerieController;
+import co.edu.uptc.controller.SubscriptionController;
 import co.edu.uptc.controller.UserController;
 import co.edu.uptc.model.Genre;
 import co.edu.uptc.model.Movie;
@@ -17,6 +18,7 @@ import co.edu.uptc.model.MultimediaContent;
 import co.edu.uptc.model.PlayList;
 import co.edu.uptc.model.Season;
 import co.edu.uptc.model.Serie;
+import co.edu.uptc.model.Subscription;
 import co.edu.uptc.model.UserRegistered;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -279,6 +281,46 @@ public class AdminView {
     @FXML
     private VBox serieOptions;
 
+    //---------------------------------------------------------------------SUBCRIPTIONS ATRIBUTES-------------------------------------------------------------//
+    @FXML
+    private AnchorPane addSubscriptionPane;
+
+    @FXML
+    private VBox subsOptions;
+
+    @FXML
+    private TextField subscriptionNameField;
+
+    @FXML
+    private Label subscriptionNameLabel;
+
+    @FXML
+    private TextField subscriptionDescriptionField;
+
+    @FXML
+    private Label subscriptionDescriptionLabel;
+
+    @FXML
+    private TextField subscriptionDurationField;
+
+    @FXML
+    private Label subscriptionDurationLabel;
+
+    @FXML
+    private TextField subscriptionPriceField;
+
+    @FXML
+    private Label subscriptionPriceLabel;
+
+    @FXML
+    private TextField subscriptionCoverField;
+
+    @FXML
+    private Label subscriptionCoverLabel;
+
+    @FXML
+    private Button createSubscriptionButton;
+
     private String selectedSeasonItem = null;
 
     private String selectedEpisodeItem = null;
@@ -302,6 +344,7 @@ public class AdminView {
     private UserController userC = new UserController();
     private SerieController serieC = new SerieController();
     private GenreController genreC = new GenreController();
+    private SubscriptionController subC = new SubscriptionController();
 
     //------------------------------------------------------------MOVIES------------------------------------------------------//
 
@@ -1579,6 +1622,323 @@ public class AdminView {
     void serieOptionsOut(MouseEvent event){
         serieOptions.setVisible(false);
     }
+
+    //---------------------------------------------------------------SUBSCRIPTIONS-------------------------------------------------------------------------------------//
+    @FXML
+    void seeAvailableSubscriptions(ActionEvent event){
+        welcomePane.setVisible(false);
+        emptyPane.setVisible(false);
+        scrollMovies.setVisible(true);
+        titleAnchorPane.setVisible(true);
+        titleText.setText("Select a Subscription to see");
+        //addMoviePane.setVisible(false);
+
+        subC.setSubscriptions(adminC.loadSubscriptionsFromJson(System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/admin-user/subscriptions.json"));
+        if(subC.getSubscriptions().isEmpty()){
+            emptyPane.setVisible(true);
+        }
+        multimediaContentGrid.getChildren().clear();
+        int column = 0;
+        int row = 0;
+
+        for (Subscription sub : subC.getSubscriptions()) {
+            VBox imageView = createSubscriptionImageView(sub);
+            imageView.setCursor(Cursor.HAND);
+            imageView.setOnMouseClicked(e -> {
+                seeSubscription(e, sub);
+            });
+            multimediaContentGrid.add(imageView, column, row);
+
+            GridPane.setHalignment(imageView, HPos.CENTER);
+            GridPane.setValignment(imageView, VPos.CENTER);
+
+            column++;
+            if (column > 3) {
+                column = 0;
+                row++;
+            }
+        }
+        
+        
+    }
+
+    public VBox createSubscriptionImageView(Subscription sub){
+        ImageView newSub = new ImageView();
+        newSub.setImage(new Image(new File(sub.getImageLocalPathURL()).toURI().toString()));
+        newSub.setId(String.valueOf(sub.getName()));
+        newSub.setFitHeight(250);
+        newSub.setFitWidth(250);
+        
+        Label subName = new Label(sub.getName());
+        subName.setTextFill(Color.BLACK);
+
+        VBox vbox = new VBox(5); 
+        vbox.setAlignment(Pos.CENTER); 
+        vbox.getChildren().addAll(newSub, subName);
+        return vbox;
+    }
+
+    public void seeSubscription(MouseEvent event, Subscription sub){
+        Stage informationSub = new Stage();
+        informationSub.initStyle(StageStyle.UTILITY); 
+        informationSub.initModality(Modality.APPLICATION_MODAL); 
+
+        Label name = new Label("Name: " + sub.getName());
+        Label description = new Label("Description: " + sub.getDescription());
+        Label duration = new Label("Label: " + sub.getDuration());
+        Label price = new Label("Price: " + sub.getPrice());
+        ImageView cover = new ImageView();
+        cover.setImage(new Image(new File(sub.getImageLocalPathURL()).toURI().toString()));
+        cover.setFitWidth(200);
+        cover.setFitHeight(200);
+        VBox advertiseWindow = new VBox(10);
+        advertiseWindow.setAlignment(Pos.CENTER);
+        advertiseWindow.getChildren().addAll(name, description, duration, price, cover);
+        Scene scene = new Scene(advertiseWindow, 210, 350);
+        informationSub.setTitle("Subscription Information");
+        informationSub.setScene(scene);
+        informationSub.showAndWait(); 
+    }
+
+    @FXML
+    void addSubscription(ActionEvent event){
+        welcomePane.setVisible(false);
+        scrollMovies.setVisible(false);
+        titleAnchorPane.setVisible(false);
+        //addMoviePane.setVisible(false);
+        //addSeriePane.setVisible(false);
+        //addGenrePane.setVisible(false);
+        emptyPane.setVisible(false);
+        addSubscriptionPane.setVisible(true);
+        
+
+    }
+
+    @FXML
+    void createSubscription(ActionEvent event){
+        int aux = subC.verifyInputs(subscriptionNameField, subscriptionNameLabel,
+                                 subscriptionDescriptionField, subscriptionDescriptionLabel, 
+                                 subscriptionDurationField, subscriptionDurationLabel, 
+                                 subscriptionPriceField, subscriptionPriceLabel, 
+                                 subscriptionCoverField, subscriptionCoverLabel);
+        if(aux == 6){
+            String path = System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/subscriptions/" + subscriptionNameField.getText().replace(" ", "") + ".png";
+            try {
+                Task<Void> task = new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        adminC.downloadCover(path, subscriptionCoverField.getText());
+                        return null;
+                    }
+                };
+                new Thread(task).start();
+                showProgressDialog(task);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+            
+            subC.createSubscription(subscriptionNameField.getText(), subscriptionDescriptionField.getText(),
+                 Integer.parseInt(subscriptionDurationField.getText()), Double.parseDouble(subscriptionPriceField.getText()),
+                  subscriptionCoverField.getText(), path);
+            adminC.saveSubscriptionsToJson(subC.getSubscriptions());
+            try {
+                adminMenu(event);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    void subscriptionsToEdit(ActionEvent event){
+        welcomePane.setVisible(false);
+        emptyPane.setVisible(false);
+        scrollMovies.setVisible(true);
+        titleAnchorPane.setVisible(true);
+        titleText.setText("Select a Subscription to edit");
+        //addMoviePane.setVisible(false);
+
+        subC.setSubscriptions(adminC.loadSubscriptionsFromJson(System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/admin-user/subscriptions.json"));
+        if(subC.getSubscriptions().isEmpty()){
+            emptyPane.setVisible(true);
+        }
+        multimediaContentGrid.getChildren().clear();
+        int column = 0;
+        int row = 0;
+
+        for (Subscription sub : subC.getSubscriptions()) {
+            VBox imageView = createSubscriptionToEditImageView(sub);
+
+            imageView.setCursor(Cursor.HAND);
+            imageView.setOnMouseClicked(e -> {
+                editSubscription(e, sub);
+            });
+            multimediaContentGrid.add(imageView, column, row);
+
+            GridPane.setHalignment(imageView, HPos.CENTER);
+            GridPane.setValignment(imageView, VPos.CENTER);
+
+            column++;
+            if (column > 3) {
+                column = 0;
+                row++;
+            }
+        }
+        
+        
+    }
+
+    public VBox createSubscriptionToEditImageView(Subscription sub){
+        ImageView newSub = new ImageView();
+        newSub.setImage(new Image(new File(sub.getImageLocalPathURL()).toURI().toString()));
+        newSub.setId(String.valueOf(sub.getName()));
+        newSub.setFitHeight(250);
+        newSub.setFitWidth(250);
+        
+        Label subName = new Label(sub.getName());
+        subName.setTextFill(Color.BLACK);
+
+        VBox vbox = new VBox(5); 
+        vbox.setAlignment(Pos.CENTER); 
+        vbox.getChildren().addAll(newSub, subName);
+        return vbox;
+    }
+
+    public void editSubscription(MouseEvent event, Subscription sub){
+        welcomePane.setVisible(false);
+        scrollMovies.setVisible(false);
+        emptyPane.setVisible(false);
+        titleAnchorPane.setVisible(false);
+        //addMoviePane.setVisible(false);
+        //addSeriePane.setVisible(false);
+        //addGenrePane.setVisible(false);
+        addSubscriptionPane.setVisible(true);
+
+        subscriptionNameField.setText(sub.getName());
+        subscriptionDescriptionField.setText(sub.getDescription());
+        subscriptionDurationField.setText(String.valueOf(sub.getDuration()));
+        subscriptionPriceField.setText(String.valueOf(sub.getPrice()));
+        subscriptionCoverField.setText(sub.getCoverURL());
+
+        createSubscriptionButton.setText("Save changes");
+        createSubscriptionButton.setOnAction(e -> {
+            int aux = subC.verifyInputsToEdit(sub.getName(), subscriptionNameField, subscriptionNameLabel, 
+                        subscriptionDescriptionField, subscriptionDescriptionLabel, subscriptionDurationField,
+                        subscriptionDurationLabel, subscriptionPriceField, subscriptionPriceLabel, subscriptionCoverField, subscriptionCoverLabel);
+            if(aux == 6){
+                sub.setName(subscriptionNameField.getText());
+                sub.setDescription(subscriptionDescriptionField.getText());
+                sub.setDuration(Integer.parseInt(subscriptionDurationField.getText()));
+                sub.setPrice(Double.parseDouble(subscriptionPriceField.getText()));
+                sub.setCoverURL(subscriptionCoverField.getText());
+                String path = System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/subscriptions/" + subscriptionNameField.getText().replace(" ", "") + ".png";
+                File oldCover = new File(sub.getImageLocalPathURL());
+                oldCover.delete();
+                try {
+                Task<Void> task = new Task<Void>() {
+                        @Override
+                        protected Void call() throws Exception {
+                            adminC.downloadCover(path, subscriptionCoverField.getText());
+                            return null;
+                        }
+                    };
+                    new Thread(task).start();
+                    showProgressDialog(task);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+                
+                sub.setImageLocalPathURL(path);
+
+                adminC.saveSubscriptionsToJson(subC.getSubscriptions());
+
+                returnToAdminMenu(e);
+            }
+        });
+    }
+
+    @FXML
+    void subscriptionsToDelete(ActionEvent event){
+        welcomePane.setVisible(false);
+        emptyPane.setVisible(false);
+        scrollMovies.setVisible(true);
+        titleAnchorPane.setVisible(true);
+        titleText.setText("Select a Subscription to delete");
+        //addMoviePane.setVisible(false);
+
+        subC.setSubscriptions(adminC.loadSubscriptionsFromJson(System.getProperty("user.dir").replace("\\", "/") + "/src/main/java/co/edu/uptc/persistence/admin-user/subscriptions.json"));
+        if(subC.getSubscriptions().isEmpty()){
+            emptyPane.setVisible(true);
+        }
+        multimediaContentGrid.getChildren().clear();
+        int column = 0;
+        int row = 0;
+
+        for (Subscription sub : subC.getSubscriptions()) {
+            VBox imageView = createSubscriptionToDeleteImageView(sub);
+            imageView.setCursor(Cursor.HAND);
+            imageView.setOnMouseClicked(e -> {
+                deleteSubscriptionFunction(sub);
+            });
+            multimediaContentGrid.add(imageView, column, row);
+
+            GridPane.setHalignment(imageView, HPos.CENTER);
+            GridPane.setValignment(imageView, VPos.CENTER);
+
+            column++;
+            if (column > 3) {
+                column = 0;
+                row++;
+            }
+        }
+        
+        
+    }
+
+    public VBox createSubscriptionToDeleteImageView(Subscription sub){
+        ImageView newSub = new ImageView();
+        newSub.setImage(new Image(new File(sub.getImageLocalPathURL()).toURI().toString()));
+        newSub.setId(String.valueOf(sub.getName()));
+        newSub.setFitHeight(250);
+        newSub.setFitWidth(250);
+        
+        Label subName = new Label(sub.getName());
+        subName.setTextFill(Color.BLACK);
+
+        VBox vbox = new VBox(5); 
+        vbox.setAlignment(Pos.CENTER); 
+        vbox.getChildren().addAll(newSub, subName);
+        return vbox;
+    }
+
+    public void deleteSubscriptionFunction(Subscription sub){
+        File oldCover = new File(sub.getImageLocalPathURL());
+            oldCover.delete();
+            subC.removeSubscription(sub);
+            adminC.saveSubscriptionsToJson(subC.getSubscriptions());
+
+            userC.setUsers(userC.loadUsersFromJson());
+            for(UserRegistered user: userC.getUsers()){
+                if(user.getSub() != null && user.getSub().getName().equals(sub.getName())){
+                    user.setSub(null);
+                }
+            }
+            
+            userC.saveUsersToJson();
+            subscriptionsToDelete(new ActionEvent());
+    }
+
+    @FXML
+    void showSubOptions(MouseEvent event){
+        subsOptions.setVisible(true);
+    }
+
+    @FXML
+    void hideSubOptions(MouseEvent event){
+        subsOptions.setVisible(false);
+    }
+
+
     //----------------------------------------------------------------FIRST AIDS-------------------------------------------------//
     @FXML
     void player(MouseEvent event, String videoPath) {
